9eb068807ba2033b32aa9a94e363f44fee34d9e2
matias.blayer <matias.blayer@stensul.com>
2018-12-01 16:25:39 -0300

    add link and script

---
+++ index.js
@@ -0,0 +1,7 @@
+(()=>{
+   const username = document.getElementsByClassName('username u-dir u-textTruncate')[1].textContent
+   const color = '#FFDF53';
+   if(username === "@Meetupjs_ar"){
+    debugger;
+   }
+})()
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Bookmarket
+
+Es una forma sensilla de agregar un boton que nos va a permitir cambiar la experiencia en el navegador.
+
